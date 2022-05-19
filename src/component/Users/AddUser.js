92929b2c04942f48b869bcd5fc6c1f7a3fa24202
vidhijain248 <vhr1317@gmail.com>
2022-05-19 19:40:32 +0530

    updated working from controlled approach to uncontrolled approach using 'refs' concept

--- src/component/Users/AddUser.js
+++ src/component/Users/AddUser.js
@@ -2,42 +2,48 @@ import React from 'react';
 import Card from '../UI/Card';
 import classes from './AddUser.module.css';
 import Button from '../UI/Button';
-import { useState } from 'react';
+import { useState , useRef} from 'react';
 import ErrorModel from '../UI/ErrorModel';
 // import Wrapper from '../Helpers/Wrapper';
 
 
 const AddUser = (props)=>{
-  const [enteredUsername,setEnteredUsername]=useState('');
-  const [enteredAge,setEnteredAge]=useState('');
+  const nameInputRef=useRef();
+  const ageInputRef=useRef();
+  // const [enteredUsername,setEnteredUsername]=useState('');
+  // const [enteredAge,setEnteredAge]=useState('');
   const [error, seterror] = useState();
 
     const addUserHandler=(event)=>{
         event.preventDefault();
-        if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0){
+        const enteredName=nameInputRef.current.value;
+        const enteredUserAge=ageInputRef.current.value;
+        if(enteredName.trim().length === 0 || enteredUserAge.trim().length === 0){
           seterror({
             title:"Invalid input",
             messages:'please enter a valid name(non-empty values)'
           });
           return;
         }
-        if(+enteredAge<1){
+        if(+enteredUserAge<1){
           seterror({
             title:"Invalid age",
             messages:'please enter a valid age(> 0)'
           });
           return;
         }
-        props.onAddUser(enteredUsername,enteredAge);
-        setEnteredUsername('');
-        setEnteredAge('');
+        props.onAddUser(enteredName,enteredUserAge);
+        nameInputRef.current.value='';
+        ageInputRef.current.value='';
+        // setEnteredUsername('');
+        // setEnteredAge('');
     };
-    const usernameChangeHandler = (event)=>{
-      setEnteredUsername(event.target.value);
-    };
-    const ageChangeHandler= (event)=>{
-      setEnteredAge(event.target.value);
-    };
+    // const usernameChangeHandler = (event)=>{
+    //   setEnteredUsername(event.target.value);
+    // };
+    // const ageChangeHandler= (event)=>{
+    //   setEnteredAge(event.target.value);
+    // };
     const errorHandler =()=>{
       seterror(null);
     }
@@ -47,9 +53,15 @@ const AddUser = (props)=>{
     <Card className={classes.input}>
     <form onSubmit={addUserHandler}>
         <label htmlFor='username'>UserName</label>
-        <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler}/>
+        <input id="username" type="text" 
+        // value={enteredUsername} 
+        // onChange={usernameChangeHandler} 
+        ref={nameInputRef}/>
         <label htmlFor='age'>Age</label>
-        <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler}/>
+        <input id="age" type="number" 
+        // value={enteredAge} 
+        // onChange={ageChangeHandler}
+         ref={ageInputRef}/>
         <Button type="submit">AddUser</Button>
     </form>
     </Card>
